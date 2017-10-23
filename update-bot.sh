4534f7e7f836c19f689fd6c4a23441dd074c50fc
Zach | xDest <destroyer7712@gmail.com>
2017-10-23 17:49:27 -0400

    Fix resetting configuration
    Since this script is usually run from the home directory, the project overwrites the contents of the "~/MDCBot" configuration folder (conflicting file names). This isolates the downloaded project into a single folder where it will cause no conflicts.

--- update-bot.sh
+++ update-bot.sh
@@ -1,5 +1,7 @@
 echo "Starting update process..."
 echo "Begin clone..."
+mkdir clone-dir
+cd clone-dir
 git clone https://github.com/MemeDistributionCo/MDCBot.git -b master
 echo "Clone attempt finished. Attempting jar creation..."
 cd MDCBot/MDCBot
@@ -9,4 +11,6 @@ mv build/libs/*.jar ~/Bots/MDCBot-latest.jar
 echo "Relocation attempt complete. Removing clone..."
 cd ../../
 rm -rf MDCBot
+../
+rm -rf clone-dir
 echo "Remove clone complete. Update attempt complete."
